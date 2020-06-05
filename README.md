7a1b27cca85ba509c3108946cd1f74de7c88f3ab
Juliet George <georgejuliet11@gmail.com>
2020-06-04 18:44:19 -0700

    image

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# password-generator
+# 03 JavaScript: Password Generator
+
+Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+
+# Coding a program that generates a password for the user.
+ 
+**Author:**
+Juliet George
+
+**Contact:**
+[LinkedIn](https://www.linkedin.com/in/juliet-george-864950b8/)
+
+**Built With**
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+## Purpose of this Project.
+
+* use the prompt feature to request user input on the browser.
+* use the if statement and while loop to validate entry.
+* create a function that combines all the characters, gets a random number of the characters, amd returns a specified length of that character. 
+* explored the ids form, password, and the tag textarea.
+
+
+Live Deployed Site:
+https://jules-boogie.github.io/password-generator/
+
+
+## License
+Licensed under MIT License
+
--- script.js
+++ script.js
@@ -5,21 +5,57 @@ var submitEl = document.queryCommandValue("#submit");
 
 
 
-//add event listener to butto
-generateBtn.addEventListener ("click", function() {
+//add event listener to button
+//generateBtn.addEventListener("click", function () {
+
+
+
+
+
+
+  //var passLength = lengthEl.value; // password length
+
+  //write function generatePassword(passLength);
+
+
+
+
+
+
+
+//})
+// PLEASE READ: 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+function generatePassword() {
+
   alert("Answer the prompts to generate a strong password!");
 
 
-  
- 
+
+
   while (true) {
     var userLength = prompt("Please enter password length from 8 to 128")
-  if(userLength >= 8 && userLength <= 128){
-    console.log(userLength)
-    break;
+    if (userLength >= 8 && userLength <= 128) {
+      //console.log(userLength)
+      break;
+    }
+    alert("please enter a length that is between 8 and 128")
   }
-  alert("please enter a length that is between 8 and 128")
-}
 
   // I tried to use the while loop to stop and repeat this prompt if condition was not met but I could not
 
@@ -30,100 +66,78 @@ generateBtn.addEventListener ("click", function() {
   var userChoice3 = prompt("do you want numeric?")
   var userChoice4 = prompt("do you want specialChar")
 
-  
 
-  var lowerCase ='abcdefghijklmnopqrstuvwxyz'
-  var upperCase ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   var numeric = '0123456789'
   var specialChar = '`~!@#$%^&*()_-=+{[}]\|:;<,.>/?'
 
   var passChar; //all characters mixed together
-  
+
 
   //there are 16 different possibilities.. I am sure that there are better ways but for my level this is ok I guess? 
-if(userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 === "yes" ){
-    passChar = lowerCase+upperCase+numeric+specialChar
-} else if(userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 === "yes"){
-  passChar = upperCase+numeric+specialChar
+  if (userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 === "yes") {
+    passChar = lowerCase + upperCase + numeric + specialChar
+  } else if (userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 === "yes") {
+    passChar = upperCase + numeric + specialChar
 
-}else if(userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 === "yes" && userChoice4 === "yes"){
-  passChar = numeric+specialChar
+  } else if (userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 === "yes" && userChoice4 === "yes") {
+    passChar = numeric + specialChar
 
-} else if(userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 === "yes"){
-  passChar = specialChar
+  } else if (userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 === "yes") {
+    passChar = specialChar
 
-}else if(userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 !== "yes"){
-  passChar = lowerCase+upperCase+numeric
+  } else if (userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 !== "yes") {
+    passChar = lowerCase + upperCase + numeric
 
-}else if(userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 !== "yes"){
-  passChar = lowerCase+upperCase
+  } else if (userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 !== "yes") {
+    passChar = lowerCase + upperCase
 
-} else if(userChoice1 === "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 !== "yes"){
-  passChar = lowerCase
+  } else if (userChoice1 === "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 !== "yes") {
+    passChar = lowerCase
 
-}else if(userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 === "yes"){
-  passChar = lowerCase+upperCase+specialChar
+  } else if (userChoice1 === "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 === "yes") {
+    passChar = lowerCase + upperCase + specialChar
 
-}else if(userChoice1 === "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 === "yes"){
-  passChar = lowerCase+specialChar
+  } else if (userChoice1 === "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 === "yes") {
+    passChar = lowerCase + specialChar
 
-}else if(userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 !== "yes"){
-  passChar = upperCase
+  } else if (userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 !== "yes") {
+    passChar = upperCase
 
-}else if(userChoice1 === "yes" && userChoice2 !== "yes" && userChoice3 === "yes" && userChoice4 !== "yes"){
-  passChar = lowerCase+numeric
+  } else if (userChoice1 === "yes" && userChoice2 !== "yes" && userChoice3 === "yes" && userChoice4 !== "yes") {
+    passChar = lowerCase + numeric
 
-}else if(userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 === "yes"){
-  passChar = upperCase+specialChar
+  } else if (userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 !== "yes" && userChoice4 === "yes") {
+    passChar = upperCase + specialChar
 
-}else if(userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 === "yes" && userChoice4 !== "yes"){
-  passChar = numeric
+  } else if (userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 === "yes" && userChoice4 !== "yes") {
+    passChar = numeric
 
-}else if(userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 !== "yes"){
-  passChar = upperCase+numeric
+  } else if (userChoice1 !== "yes" && userChoice2 === "yes" && userChoice3 === "yes" && userChoice4 !== "yes") {
+    passChar = upperCase + numeric
 
-}else if(userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 !== "yes"){
-  alert("Please choose an option to continue")
-
-}
-
-
-//while(userLength >= 8 && userLength <= 128){
-  //var passLen = userLength
-//} 
-
-
-//var passLength = lengthEl.value; // password length
-
-//write function generatePassword(passLength);
- function generatePassword(userLength){
-  var value = "";
-  for(var i = 0; i <= userLength-1; i++){
-    value += passChar.charAt(Math.floor(Math.random()* passChar.length));
+  } else if (userChoice1 !== "yes" && userChoice2 !== "yes" && userChoice3 !== "yes" && userChoice4 !== "yes") {
+    alert("Please choose an option to continue")
 
   }
-return value;
 
- }
-generatePassword(userLength)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword(userLength);
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+
+
+
+  var value = "";
+  console.log(userLength)
+  for (var i = 0; i < userLength; i++) {
+    value += passChar.charAt(Math.floor(Math.random() * passChar.length));
+
+  }
+  return value;
 
 }
-
-writePassword()
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-})
-// PLEASE READ: 
-
-
 
 //pseudocode : thinking through the assignment.  Alot changes during execution. 
 /*
@@ -144,7 +158,7 @@ uppercase: random A-Z
 numeric: random 1-9
 specialcase = random punctuation sign
 
-6) generate password 
+6) generate password
 password  = lowercase+uppercase+numeric+special case
 password = shuffle(password)
 where shuffle is to generate random numbers from password
